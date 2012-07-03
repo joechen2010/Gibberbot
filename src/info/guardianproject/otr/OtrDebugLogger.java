cc2e46cb3f282d66f806658e63bbc6175f0a8278
Joe Chen <chenzhenguang@139.com>
2012-07-03 13:18:02 +0800

    init

---
+++ src/info/guardianproject/otr/OtrDebugLogger.java
@@ -0,0 +1,23 @@
+package info.guardianproject.otr;
+
+import android.util.Log;
+
+public class OtrDebugLogger {
+
+
+	private final static String TAG = "Gibberbot.OTR";
+	public static boolean debugLog = true;
+	public static boolean errorLog = true;
+	
+	public static void log (String msg)
+	{
+		if (debugLog)
+			Log.d(TAG, msg);
+	}
+	
+	public static void log (String msg, Exception e)
+	{
+		if (errorLog)
+			Log.e(TAG, msg, e);
+	}
+}
